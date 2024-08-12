94f89e85a93291496a41d3d9b288967dbeec715d
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-08-12 20:59:19 +0300

    adding the gameId to game class and methods that get players's ids

--- server/src/lib/game.ts
+++ server/src/lib/game.ts
@@ -2,23 +2,29 @@
 // when create new game -> pass the two palyers together to the game controller
 import Player from './player';
 import { type Coord } from './types';
+import { v4 as uuidv4 } from 'uuid';
 
 class Game {
     private player1: Player;
     private player2: Player;
-    // active player how recvies attack
     private activePlayer: Player;
     private nonActivePlayer: Player;
+    private gameId: string;
 
     constructor(player1: Player, player2: Player) {
         this.player1 = player1;
         this.player2 = player2;
         this.activePlayer = player1;
         this.nonActivePlayer = player2;
+        this.gameId = uuidv4();
     }
 
-    private isGameOver(): boolean {
+    public isGameOver(): boolean {
         return this.player1.hasLost() || this.player2.hasLost();
+    }
+
+    public getGameId(): string {
+        return this.gameId;
     }
 
     public playRound(cord: Coord, id: string): boolean {
@@ -36,11 +42,19 @@ class Game {
     public getWinner(): string | undefined {
         if (this.isGameOver()) {
             if (this.player1.hasLost()) {
-                return this.player2.getId();
+                return this.player2.getName()
             } else {
-                return this.player1.getId();
+                return this.player1.getName()
             }
         }
+    }
+
+    public getNonActivePlayerId(): string {
+        return this.nonActivePlayer.getId();
+    }
+
+    public getActivePlayerId(): string {
+        return this.activePlayer.getId();
     }
 
     private switchPlayers(): void {
