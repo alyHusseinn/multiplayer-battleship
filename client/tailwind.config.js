47df4956186b5d112f1b33156b36ea8cff7cd0b2
Aly Hussein <111089237+alyHusseinn@users.noreply.github.com>
2024-08-19 23:19:16 +0300

    add new colors to tailwindConfig

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,6 +9,10 @@ export default {
       colors: {
         'primary': 'rgb(209, 207, 213)',
         'secondary': 'rgb(77, 59, 81)',
+        'sec-gray': '#b8b2b2',
+        'ship': 'rgb(181, 103, 103)',
+        'hit': 'rgb(137, 190, 137)',
+        'not-hi': 'rgb(170, 165, 165)',
       }
     },
   },
